128c799d5812f48a48f571b6698d6d5073bec162
Dig-Bick <1239343496@qq.com>
2023-04-09 16:50:00 +0800

    catpostqian

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import UserManagement from '@/views/UserManagement.vue'; // 引入用户管理页面组件
 import AdminLogin from '@/views/AdminLogin.vue';
 import AdminRegister from '@/views/AdminRegister.vue';
 import Management from '@/views/Management.vue';
+import PostList from "../components/PostList.vue";
+import PostDetails from "../components/PostDetails.vue";
+import CategoryList from "../components/CategoryList.vue";
+import CategoryDetails from "../components/CategoryDetails.vue";
 
 const routes = [
   // {
@@ -50,6 +53,26 @@ const routes = [
   name: 'Management',
   component: Management,
 },
+{
+    path: "/posts",
+    name: "post-list",
+    component: PostList,
+  },
+  {
+    path: "/posts/:id",
+    name: "post-details",
+    component: PostDetails,
+  },
+  {
+    path: "/categories",
+    name: "category-list",
+    component: CategoryList,
+  },
+  {
+    path: "/categories/:id",
+    name: "category-details",
+    component: CategoryDetails,
+  },
 ];
 
 const router = createRouter({
